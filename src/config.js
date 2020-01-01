53173c807fd6e6542160030e0764d889f618ba1c
Chris Hurley <cwhurley94@gmail.com>
2020-01-01 12:54:22 +1100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uploads-chris"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://9r4ygwkq0i.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_CpCrOTNNj",
+        APP_CLIENT_ID: "5vgj3evq7ej30s4sat9ru9clee",
+        IDENTITY_POOL_ID: "us-east-2:bea760ae-4b83-42f6-a1f4-72d5a425d8d2"
+    }
+};
