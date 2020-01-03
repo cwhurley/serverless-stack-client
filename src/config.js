40b05cdadeb3f5fbb11308101bb359171473f876
Chris Hurley <cwhurley94@gmail.com>
2020-01-03 11:18:01 +1100

    Building our react app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_ofinnw5oh0OBXhVHTEoqZmjr00DBVTesWK",
     s3: {
         REGION: "us-east-1",
         BUCKET: "notes-app-uploads-chris"
